171b9834af5838c37183f4051161659d860012e2
keito-sakakibara <keito122324@icloud.com>
2021-02-10 14:38:29 +0900

    add_show_new_edit_spec

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,20 +1,64 @@
 require 'rails_helper'
 
-RSpec.describe TasksController, type: :controller do
-  describe "#index" do
-    it "正常なレスポンスか" do
-      get :index
+RSpec.describe TasksController, type: :request do
+  describe '#index' do
+    subject { get tasks_path }
+    it 'リクエストが成功すること' do
+      subject
       expect(response).to be_success
+      expect(response).to have_http_status(200)
     end
-    # 200レスポンスが返ってきているか？
-    it "200レスポンスが返ってきているか" do
-      get :index
-      expect(response).to have_http_status "200"
+  end
+  describe '#show' do
+    context "タスクが存在する時" do
+      let!(:task1) { create(:task1) }
+      subject { get task_path task1.id }
+  
+      it 'リクエストが成功すること' do
+        subject
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+  
+      it 'タスク名が表示されていること' do
+        subject
+        expect(response.body).to include 'task1'
+      end
     end
-    it "taskのカウント数があっているか" do
-      task1 = create(:task1)
-      task2 = create(:task2)
-      expect(Task.count).to eq(2)
+
+    context "タスクが存在しない時" do
+      subject { -> {get task_path 5} }
+
+      it { is_expected.to raise_error ActiveRecord::RecordNotFound }
+    end
+  end
+
+  describe '#new' do
+    subject { get new_task_path }
+
+    it "リクエストが成功すること" do
+      subject
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe '#edit' do
+    let!(:task1) { create(:task1) }
+    subject { get edit_task_path task1 }
+
+    it "リクエストが成功すること" do
+      subject
+      expect(response.status).to eq 200
+    end
+
+    it "タスク名が表示されていること" do
+      subject
+      expect(response.body).to include "task1"
+    end
+
+    it "詳細が表示されていること" do
+      subject
+      expect(response.body).to include "task1です"
     end
   end
 end
