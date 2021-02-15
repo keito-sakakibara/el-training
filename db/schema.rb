6cdcff50c078f0ab1e9ff5aa2eae20952acef2d9
keito-sakakibara <keito122324@icloud.com>
2021-02-15 14:52:52 +0900

    add_Notnull

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_083522) do
+ActiveRecord::Schema.define(version: 2021_02_15_054736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "detail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
