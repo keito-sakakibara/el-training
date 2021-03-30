0930a3b0ad4e174f03fa7722adfb63f8be36da04
keito-sakakibara <keito122324@icloud.com>
2021-03-30 12:41:27 +0900

    add_admin_users__request_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   def leave_at_least_one_admin_user
     unless User.where(is_admin: true).count > 1
-      errors.add(:is_admin, "は最低1人必要です")
+      errors.add(:is_admin, 'は最低1人必要です')
       throw(:abort)
     end
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,5 +6,13 @@ FactoryBot.define do
     email { 'test@test.com' }
     password { 'password' }
     password_confirmation { 'password' }
+    is_admin { false }
+  end
+  trait :admin do
+    name { 'admin' }
+    email { 'admin@admin.com' }
+    password { 'admin' }
+    password_confirmation { 'admin' }
+    is_admin { true }
   end
 end
